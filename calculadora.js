8e4538f0381d5e1ea093f847c4572dd226aa144e
dudapascoall <142435751+dudapascoall@users.noreply.github.com>
2024-02-22 12:03:13 -0300

    Add files via upload

---
+++ calculadora.js
@@ -0,0 +1,7 @@
+const calcular = () => {
+    let numero1 = document.getElementById("numero1");
+    let numero2 = document.getElementById("numero2");
+    let numero3 = document.getElementById("numero3");
+
+    resultado.value = parseInt(numero1.value) + parseInt(numero2.value);
+}
